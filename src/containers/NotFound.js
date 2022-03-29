5fa505dbc68214051444b45cc96ff42f3addff6c
Habib <hbrawnak@gmail.com>
2022-03-29 17:36:51 +0600

    404 component updated

--- src/containers/NotFound.js
+++ src/containers/NotFound.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 const NotFound = () => {
-  return (
-      <div>
-          <h2>404 Not Found!</h2>
-      </div>
-  );
+    return (
+        <div className="ui grid container">
+            <div className="four wide column">
+                <h2>404 Not Found!</h2>
+            </div>
+        </div>
+    );
 }
 
 export default NotFound;
